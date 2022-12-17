9ec6f31faebbfd35350a6fd60f3149c4d3020610
Sach7n <sachinmacwan07@gmail.com>
2022-12-16 20:12:14 -0500

    almost finished

--- src/Components/general/ResponsiveSection.tsx
+++ src/Components/general/ResponsiveSection.tsx
@@ -11,9 +11,9 @@ type secProps = {
 const style: SxProps<Theme> = {
     height: "90%",
     width: "90vw",
-    margin: "3% 2%",
+    margin: "4% 2% 0 2%",
     boxShadow: 5,
-    background: "radial-gradient(circle, rgba( 1, 35, 36,0.9) 80%,rgba(2,40,40,1) 100%)",
+    background: "radial-gradient(circle, rgb(15, 23, 36,0.9) 80%,rgba(12,44,48,0.5) 100%)",
     border: "3px solid transperent",
     borderRadius: "30px",
     paddingTop: "2vh"
--- src/Theme.tsx
+++ src/Theme.tsx
@@ -1,7 +1,16 @@
 import { createTheme } from '@mui/material/styles';
 
 const mainTheme = createTheme({
-  
+  // components: {
+  //   MuiCssBaseline: {
+  //     styleOverrides: {
+  //       body: {
+  //         backgroundColor: "#FAACA8",
+  //         backgroundImage: `linear-gradient(19deg, #FAACA8 0%, #DDD6F3 100%)`,
+  //       },
+  //     },
+  //   },
+  // },
     palette: {
       primary:{
         main:"#fff"
